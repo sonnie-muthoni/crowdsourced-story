675ed9d2941d50544a255cb6ced5fa113119d5f1
Marris_Muthoni <81642933+sonnie-muthoni@users.noreply.github.com>
2024-10-17 17:04:46 +0300

    Delete backend directory

--- backend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStories, addContribution } = require('./controllers');
-
-router.get('/stories', getStories);
-router.post('/contributions', addContribution);
-
-module.exports = router;
